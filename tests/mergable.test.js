c87c44599e2bd5b78446b60ac65db36382f213c2
Hazdryx <hazdryx@gmail.com>
2021-07-14 06:42:17 -0500

    Added tests for when objects should be mergable.

--- tests/mergable.test.js
+++ tests/mergable.test.js
@@ -1,4 +1,12 @@
 const mergable = require('../src/mergable');
+class A {
+    constructor(x) {
+        this.x = x;
+    }
+    double() {
+        return 2 * this.x;
+    }
+}
 /**
  * Runs a test function using mergable.
  * 
@@ -14,15 +22,6 @@ function runTest(description, any, result) {
 }
 
 describe('Not Mergable', () => {
-    class A {
-        constructor(x) {
-            this.x = x;
-        }
-        double() {
-            return 2 * this.x;
-        }
-    }
-
     runTest('empty object', { }, false);
     runTest('array', [], false);
     runTest('date', new Date(), false);
@@ -30,4 +29,13 @@ describe('Not Mergable', () => {
     runTest('classes', A, false);
     runTest('class instances', new A(1), false);
     runTest('objects /wo functions', { hello: "world" }, false);
+    runTest('objects /w class instances', { a: new A(2) }, false);
+});
+describe('Mergable', () => {
+    runTest('objects /w classes', { A: A }, true);
+    runTest('objects /w functions', { runTest: runTest }, true);
+    runTest('objects /w nested classes', { ns: { A: A }}, true);
+    runTest('objects /w nested functions', { testing: { runTest: runTest }}, true);
+    runTest('objects /w data and functions', { x: 5, runTest: runTest }, true);
+    runTest('objects /w data and nested functions', { x: 5, testing: { runTest: runTest }}, true);
 });
